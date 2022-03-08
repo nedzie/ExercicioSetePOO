6881822ded959e0dfb5df9995f522119f83eb60a
nedzie <marcos-elivelton@hotmail.com>
2022-03-08 15:56:59 -0300

    Add project files.

---
+++ ExercicioSete.ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ExercicioSete.ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Vendedor marcos = new Vendedor();
+            Console.Write("Informe o salário base do vendedor: \n> R$");
+            marcos.salarioBase = double.Parse(Console.ReadLine());
+            Console.Write("Informe o total de vendas dele: \n> R$");
+            marcos.totalDeVendas = double.Parse(Console.ReadLine());
+            Console.Write("Informe o percentual de comissão: \n> ");
+            marcos.percentualDeComissao = double.Parse(Console.ReadLine());
+
+            marcos.salarioTotal = marcos.CalcularSalarioTotal(marcos.salarioBase, marcos.totalDeVendas, marcos.percentualDeComissao);
+
+            Console.WriteLine("O salário total do vendedor é de: R$" + marcos.salarioTotal + ".");
+        }
+    }
+}
---
+++ ExercicioSete.ConsoleApp/Vendedor.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercicioSete.ConsoleApp
+{
+    internal class Vendedor
+    {
+        public double salarioBase;
+        public double totalDeVendas;
+        public double percentualDeComissao;
+        public double salarioTotal;
+
+        public double CalcularSalarioTotal(double salarioBase, double totalDeVendas, double percentualDeComissao)
+        {
+            salarioTotal = salarioBase + (totalDeVendas * (percentualDeComissao / 100));
+            return salarioTotal;
+        }
+    }
+}
